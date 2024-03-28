e7d6e5d1a54e1a7c5bf9a3c76fd7b81eb7b82f2c
dimkhot <dmytro.khotlubiei@gmail.com>
2024-03-28 18:29:15 +0200

    homework 3 whitout comments

---
+++ src/main/java/org/example/homework3/DivisionRemainder.java
@@ -0,0 +1,12 @@
+package org.example.homework3;
+
+public class DivisionRemainder {
+    public static void main (String[] args){
+        int dividend = 23;
+        int divisor = 3;
+        int division = dividend / divisor;
+        int reminder = dividend % divisor;
+        System.out.println("Integer Part: " + division);
+        System.out.println("Reminder is: " + reminder);
+    }
+}
---
+++ src/main/java/org/example/homework3/MinMax.java
@@ -0,0 +1,14 @@
+package org.example.homework3;
+
+public class MinMax {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 7;
+
+        int min = Math.min(a, b);
+        int max = Math.max(a, b);
+
+        System.out.println("Minimum: " + min);
+        System.out.println("Maximum: " + max);
+    }
+}
